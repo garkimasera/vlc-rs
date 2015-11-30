b376a0de12aa11e4be3af2dc24f2ff33538ba7b4
T. Okubo <t.okubo.rx78+github@gmail.com>
2015-11-30 23:04:45 +0900

    Add methods to MediaLibrary

--- src/media_library.rs
+++ src/media_library.rs
@@ -3,9 +3,35 @@
 // Licensed under the MIT license, see the LICENSE file.
 
 use ffi;
+use ::{Instance, MediaList};
 
 pub struct MediaLibrary {
     pub ptr: *mut ffi::libvlc_media_library_t,
+}
+
+impl MediaLibrary {
+    /// Create an new Media Library object.
+    pub fn new(instance: &Instance) -> Option<MediaLibrary> {
+        unsafe{
+            let p = ffi::libvlc_media_library_new(instance.ptr);
+            if p.is_null() { None }else{ Some(MediaLibrary{ptr: p}) }
+        }
+    }
+
+    /// Load media library.
+    pub fn load(&self) -> Result<(), ()> {
+        unsafe{
+            if ffi::libvlc_media_library_load(self.ptr) == 0 { Ok(()) }else{ Err(()) }
+        }
+    }
+
+    /// Get media library subitems.
+    pub fn media_list(&self) -> Option<MediaList> {
+        unsafe{
+            let p = ffi::libvlc_media_library_media_list(self.ptr);
+            if p.is_null() { None }else{ Some(MediaList{ptr: p}) }
+        }
+    }
 }
 
 impl Drop for MediaLibrary {
