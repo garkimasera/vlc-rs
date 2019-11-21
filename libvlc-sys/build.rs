0417f9e2e83ec7d30f95464b9fc7d71666d7706c
T. Okubo <t.okubo.rx78+devel@gmail.com>
2019-11-21 20:00:26 +0900

    Add libvlc-sys crate

---
+++ libvlc-sys/build.rs
@@ -0,0 +1,24 @@
+use bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rustc-link-lib=libvlc");
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        // For no_std
+        .use_core()
+        // Use libc
+        .ctypes_prefix("libc")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ libvlc-sys/wrapper.h
@@ -0,0 +1,1 @@
+#include <vlc/vlc.h>
